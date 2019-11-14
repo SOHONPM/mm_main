447a652ff801c89c584b08ee917a2c88539219e9
SOHONPM <support@smartonhold.com.au>
2019-11-14 10:32:27 +1000

    commit

--- template-parts/modules/videos.php
+++ template-parts/modules/videos.php
@@ -1,6 +1,9 @@
 <section class="video-section">
     <div class="container">
         <div class="row">
+            <div class="col-md-12">
+                <h2 class="main-title"><?php echo get_sub_field('title') ?></h2>
+            </div>
             <?php
 
             // check if the flexible content field has rows of data
