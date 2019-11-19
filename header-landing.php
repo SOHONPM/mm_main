ec3288c34852ccd4d3a9a6ee076cb8aba1a771da
SOHONPM <support@smartonhold.com.au>
2019-11-19 14:25:19 +1000

    commit

--- header-landing.php
+++ header-landing.php
@@ -70,68 +70,78 @@ if (!defined('ABSPATH')) {
     <!-- ./Preloader -->
     <!-- Site-header -->
     <header id="landing" class="site-header" itemscope itemtype="http://schema.org/WPHeader">
-        <div class="site-header__middle">
+        <div class="site-header__top">
             <div class="container">
+                <div class="grid">
+                    <div class="col--right">
+                        <div class='header-info'>
 
-                <!-- Site-logo -->
-                <div class="site-logo">
-                    <a href="<?php echo site_url() ?>"><?php themezero_get_logo() ?></a>
-                </div>
-                <div class="col-md">
+                            <div class="col-md-12">
+                                <div class="wrapper">
+                                    <p class="info button-dropdown " data-toggle="modal" data-target="#email"><?php echo "<span class='email'><i class='fas fa-envelope'></i> " . get_theme_mod('header_info') . "</span>" ?></p>
 
-                    <div class="countdown-timer">
+                                    <!-- Modal -->
+                                    <div class="modal fade bd-example-modal-lg" id="email" tabindex="-1" role="dialog" aria-labelledby="emailLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
 
-                        <div class="row">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="form__horizontal">
+                                                        <h1 class="form-title">Email Us</h1>
+                                                        <?php echo do_shortcode('[gravityform id="16" title="true" description="true" ajax="true"]') ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
 
-                            <div class="col with-separator">
+                                    <div class="call">
+                                        <a class="info" href="tel:<?php echo get_theme_mod('phone') ?>"><?php echo "<span class='call-us'><i class='fas fa-phone-alt'></i> " . get_theme_mod('header_info_2') . "</span>" ?></a>
 
-                                <div class="countdown-timer-days">
+                                        <?php
+                                        wp_nav_menu(array(
+                                            'depth'             => 1,
+                                            'theme_location' => 'social',
+                                            'menu_class'      => 'social-links',
+                                            'container'       => false,
+                                            'container_class' => false,
+                                            'container_id'    => false,
+                                            'walker'          => new WP_Bootstrap_Navwalker(),
+                                        ));
 
-                                    <strong></strong>
-                                    <span>Days</span>
-
+                                        ?>
+                                    </div>
                                 </div>
-
                             </div>
-                            <div class="col with-separator">
-
-                                <div class="countdown-timer-hours">
-
-                                    <strong></strong>
-                                    <span>Hours</span>
-
-                                </div>
-
-                            </div>
-                            <div class="col with-separator">
-
-                                <div class="countdown-timer-minutes">
-
-                                    <strong></strong>
-                                    <span>Minutes</span>
-
-                                </div>
-
-                            </div>
-                            <div class="col">
-
-                                <div class="countdown-timer-seconds">
-
-                                    <strong></strong>
-                                    <span>Seconds</span>
-
-                                </div>
-
-                            </div>
-
                         </div>
-
                     </div>
-
                 </div>
-                <!-- ./Site-logo -->
             </div>
-        </div>
+            <div class="site-header__middle">
+                <div class="container">
+                    <!-- Site-logo -->
+                    <div class="site-logo">
+                        <a href="<?php echo site_url() ?>"><?php themezero_get_logo() ?></a>
+                    </div>
+                    <!-- ./Site-logo -->
+                    <div class="grid">
+                        <div class="col--right">
+                            <div class='header-info'>
+                                <div class="col-md-12">
+                                    <div class="count">
+                                        <p><span>10</span> out of 12 spots left!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </header>
     <!-- ./Site-header -->
     <!-- Accessibility -->
