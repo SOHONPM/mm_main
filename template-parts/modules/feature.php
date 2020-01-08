d02a026ea2f1c0ae77838020abec3cf710bdfc5e
SOHONPM <support@smartonhold.com.au>
2020-01-08 15:24:55 +1000

    commit

--- template-parts/modules/feature.php
+++ template-parts/modules/feature.php
@@ -1,20 +1,25 @@
 <section class="feature-section">
     <div class="container">
-        <?php if(get_sub_field('title')): ?>
-        <div class="col-md-12">
-            <h2 class="title"><?php echo get_sub_field('title')?></h2>
-        </div>
+        <?php if (get_sub_field('title')) : ?>
+            <div class="col-md-12">
+                <h2 class="title"><?php echo get_sub_field('title') ?></h2>
+            </div>
         <?php endif ?>
         <div class="row">
             <?php
             foreach (get_sub_field('grid') as $value) {
-                ?>
+            ?>
                 <div class="col-md-4 text-center">
                     <div class="single-feature">
                         <div class="wrapper">
+                            <?php if ('image_position == "top') : ?>
+                                <img src="<?php echo $value['icon']['url'] ?>" alt="">
+                            <?php endif ?>
                             <p class="title"><?php echo $value['title'] ?></p>
                             <p class="content"><?php echo $value['content'] ?></p>
-                            <img src="<?php echo $value['icon']['url'] ?>" alt="">
+                            <?php if ('image_position == "bottom') : ?>
+                                <img src="<?php echo $value['icon']['url'] ?>" alt="">
+                            <?php endif ?>
                         </div>
                     </div>
                 </div>
