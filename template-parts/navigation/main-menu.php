a5c6c43a0564b21ac4e1853957e4fa92bf06de1a
SOHONPM <support@smartonhold.com.au>
2020-01-24 11:31:46 +1000

    commit

--- template-parts/navigation/main-menu.php
+++ template-parts/navigation/main-menu.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Display Navigation 
@@ -12,39 +12,39 @@
 
 ?>
 
-<?php if( has_nav_menu('top') ) : ?>
+<?php if (has_nav_menu('top')) : ?>
 
-<nav itemscope itemtype="http://schema.org/SiteNavigationElement" class="navbar navbar-expand-md" role="navigation">
-	<div class="navbar__mobile">
-	    <a class="navbar-brand" href="#">Menu</a>
-	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon">
-		    	<svg width="35.477" height="27.916" viewBox="0 0 35.477 27.916" id="burgerMenu">
-				  <g id="Group_26" data-name="Group 26" transform="translate(-361 -79)">
-				    <rect id="Rectangle_64" data-name="Rectangle 64" width="35.477" height="6.979" rx="3.49" transform="translate(361 79)" fill="#0e2357"/>
-				    <rect id="Rectangle_65" data-name="Rectangle 65" width="35.477" height="6.979" rx="3.49" transform="translate(361 89.469)" fill="#0e2357"/>
-				    <rect id="Rectangle_66" data-name="Rectangle 66" width="35.477" height="6.979" rx="3.49" transform="translate(361 99.937)" fill="#0e2357"/>
-				  </g>
-				</svg>
-		    </span>
-		</button>
-	</div>
+	<nav itemscope itemtype="http://schema.org/SiteNavigationElement" class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+		<div class="navbar__mobile">
+			<a class="navbar-brand" href="#">Menu</a>
+			<button class="hamburger animated fadeInLeft is-closed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon">
+					<svg width="35.477" height="27.916" viewBox="0 0 35.477 27.916" id="burgerMenu">
+						<g id="Group_26" data-name="Group 26" transform="translate(-361 -79)">
+							<rect id="Rectangle_64" data-name="Rectangle 64" width="35.477" height="6.979" rx="3.49" transform="translate(361 79)" fill="#0e2357" />
+							<rect id="Rectangle_65" data-name="Rectangle 65" width="35.477" height="6.979" rx="3.49" transform="translate(361 89.469)" fill="#0e2357" />
+							<rect id="Rectangle_66" data-name="Rectangle 66" width="35.477" height="6.979" rx="3.49" transform="translate(361 99.937)" fill="#0e2357" />
+						</g>
+					</svg>
+				</span>
+			</button>
+		</div>
 
-     <?php 
+		<?php
 
-        wp_nav_menu( array(
-		    'menu'           => 'Primary Menu', // Do not fall back to first non-empty menu.
-		    'depth'	         => 2,
-		    'theme_location' => 'top',
-		    'menu_class' 	 => 'nav navbar-nav main-menu',
-		    'container'       => 'div',
+		wp_nav_menu(array(
+			'menu'           => 'Primary Menu', // Do not fall back to first non-empty menu.
+			'depth'	         => 2,
+			'theme_location' => 'top',
+			'menu_class' 	 => 'nav sidebar-nav',
+			'container'       => 'div',
 			'container_class' => 'collapse navbar-collapse',
 			'container_id'    => 'navbarSupportedContent',
 			'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
 			'walker'          => new WP_Bootstrap_Navwalker(),
-		)); 
+		));
 
-     ?>
-</nav>
+		?>
+	</nav>
 
 <?php endif; ?>
